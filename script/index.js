4008c53e6700f5142445e86489a4f9dadfbd5373
mahmudur2015 <sumonmahmud.2015@gmail.com>
2025-07-14 12:07:50 +0600

    video details section added

--- script/index.js
+++ script/index.js
@@ -16,7 +16,7 @@ function loadCategories() {
 
 // Display video categories
 function displayCategories(categories) {
-   
+
     const categoryContainer = document.getElementById("category-container");
 
     for (const cate of categories) {
@@ -31,15 +31,13 @@ function displayCategories(categories) {
     }
 }
 
-
-
 function loadVideos() {
     fetch("https://openapi.programming-hero.com/api/phero-tube/videos")
         .then((res) => res.json())
         .then((data) => {
 
             removeActiveClass();
-            document.getElementById("btn-all").classList.add("active");          
+            document.getElementById("btn-all").classList.add("active");
             displayVideos(data.videos);
         });
 }
@@ -85,6 +83,7 @@ const displayVideos = (videos) => {
                     <p class="text-sm text-gray-400">${video.others.views} views</p>
                 </div>
             </div>
+            <button onclick="loadVideoDetails('${video.video_id}')" class="btn btn-block">Show Details</button>
         </div> 
         
         `;
@@ -105,16 +104,63 @@ const loadCategoryVideos = (id) => {
         .then((res) => res.json())
         .then((data) => {
 
-             removeActiveClass();
+            removeActiveClass();
 
             const clickedButton = document.getElementById(`btn-${id}`);
             clickedButton.classList.add("active");
-            
+
             displayVideos(data.category)
         });
 }
 
-//Display Videos
+const loadVideoDetails = (videoid) => {
+    const url = `https://openapi.programming-hero.com/api/phero-tube/video/${videoid}`
+    fetch(url)
+        .then((res) => res.json())
+        .then(data => displayVideoDetails(data.video));
+}
+
+const displayVideoDetails = (video) => {
+
+    console.log(video);
+    document.getElementById("video_details").showModal()
+
+    const detailsContainer=document.getElementById("details-container");
+    detailsContainer.innerHTML=`
+    <div class="card bg-base-100  shadow-sm">
+  <figure>
+    <img
+      src="${video.thumbnail}"
+      alt="thubnail" />
+  </figure>
+  <div class="card-body">
+    <h2 class="card-title">
+      <div class="badge badge-secondary">${video.title}</div>
+    </h2>
+    <p>${video.description}</p>
+    <div class="card-actions justify-end">
+    <div class="profile">
+                    <div class="avatar">
+                        <div class="ring-primary ring-offset-base-100 w-6 rounded-full ring-2 ring-offset-2">
+                            <img src="${video.authors[0].
+                profile_picture
+            }" />
+                        </div>
+                    </div>
+                </div>
+      <div class="badge badge-outline">
+      <p class="text-sm text-black flex gap-1">${video.authors[0].
+                profile_name}</p>
+      </div>
+    </div>
+  </div>
+</div>
+
+    `
+
+
+}
+
 
 
 
